f48cb346ccc665b7fcd478270a61b7fd3b88313f
jturner1120 <jeff.thomas.turner@gmail.com>
2017-05-24 19:18:07 -0400

    bootstrap and coverpage work

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Burnt Code' });
+  res.render('index', { title: 'Burnt Code', layout: 'coverlayout.hbs', layoutDir: __dirname + "/views/layouts"});
 });
 
 module.exports = router;
